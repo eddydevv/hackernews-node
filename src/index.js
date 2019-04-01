df6bf0fff0003f85ce7bb2d033fe16d14505a658
Edwin Agbenyega <eddybanks03@gmail.com>
2019-04-01 14:17:18 -0600

    Add post mutation for links

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
-
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
+
+let idCount = links.length
 
 const resolvers = {
   Query: {
@@ -25,15 +14,21 @@ const resolvers = {
     feed: () => links,
   },
 
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 
